12de5cb482e6de8560cff8a2d07b037685fe16e5
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-08-22 20:54:56 -0300

    primer commit, proyecto configurado, pero no funciona

---
+++ src/gdexample.cpp
@@ -0,0 +1,26 @@
+#include "gdexample.h"
+
+using namespace godot;
+
+void GDExample::_register_methods()
+{
+    register_method("_process", &GDExample::_process);
+}
+
+GDExample::GDExample() {
+}
+
+GDExample::~GDExample() {
+    // add your cleanup here
+}
+
+void GDExample::_init() 
+{
+    // initialize any variables here
+    time_passed = 0.0;
+    Godot::print("hola mundo desde C++");   
+}
+
+void GDExample::_process(float delta)
+{
+}
